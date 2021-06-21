24d681ba3a233324233ac33ad7fb337ba577370e
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2021-06-21 03:40:00 +0000

    made final answers

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,8 +5,8 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "Who was the first American woman in space? "
+let correctAnswer = 'Sally Ride';
 let candidateAnswer;
 let questions;
 let correctAnswers;
@@ -15,19 +15,23 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question('What is your name? ')
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = input.question(question)
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if (candidateAnswer === correctAnswer){
+  console.log("Your answer was correct.")
+} else {
+  console.log("Your answer was incorrect.")
+}
 
   let grade;
   
@@ -38,7 +42,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log(`\nHello ${candidateName}, it's nice to meet you.`)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
