fc7367ff9d41a9922c8eb61ae8dd92f513bd6796
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2021-06-22 00:37:26 +0000

    finished part 2, defined allAnswers to accommodate part 1.2c

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,6 +1,9 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
+
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
@@ -8,10 +11,10 @@ let candidateName;
 let question = "Who was the first American woman in space? "
 let correctAnswer = 'Sally Ride';
 let candidateAnswer;
-let questions;
-let correctAnswers;
+//let questions;
+//let correctAnswers;
 let candidateAnswers;
-
+let allAnswers = []
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -20,18 +23,30 @@ candidateName = input.question('What is your name? ')
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
 
+
+for (i = 0; i < questions.length; i++){
+  candidateAnswers = input.question(questions[i]);
+  allAnswers.push(candidateAnswers)
 }
-
+}
+//console.log(allAnswers)
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer){
-  console.log("Your answer was correct.")
-} else {
-  console.log("Your answer was incorrect.")
+
+
+//console.log(allAnswers)
+for (i = 0; i < correctAnswers.length; i++){
+  console.log(`\nYour answer: ${allAnswers[i]} \nCorrect answer: ${correctAnswers[i]}`)
 }
+
+
+// if (candidateAnswers === correctAnswers){
+//   console.log("Your answer was correct.")
+// } else {
+//   console.log("Your answer was incorrect.")
+// }
 
   let grade;
   
